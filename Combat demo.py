1ae0229131bff3ed5546e5b4e0b062de4c2b72a4
Anasaii <65514860+Anasaii@users.noreply.github.com>
2023-04-20 14:30:43 -0400

    Update Combat demo.py
    school pc editing :))))

--- Combat demo.py
+++ Combat demo.py
@@ -4,7 +4,7 @@ health = 100
 items = {}
 weapons = {}
 
-def buttonQuery(buttonState, list, listname, listdesc):
+def pickUpOption(buttonState, list, listname, listdesc):
     if buttonState == "yes":
         list[listname] = listdesc
         buttonState = None
@@ -12,23 +12,33 @@ def buttonQuery(buttonState, list, listname, listdesc):
         buttonState = None
         pass
 
-def spider.self
+
+class enemy():
+    def __init__(self, health, damage):
+        self.health = health
+        self.damage = damage
+    
+    def Attack():
+        print("Hello i attack you for" + enemy.damage)
+
+
+    
+spider = enemy("10","3")
+spider.Attack()
+
 class Game:
     print("You find a weapon!")
-
+    
     buttonState = input("Do you pick up the weapon?")
-    buttonQuery(buttonState, weapons, "flimsyWeapon", 50)
+    pickUpOption(buttonState, weapons, "flimsyWeapon", 50)
 
     print(weapons)
     print("magically you also find stick")
 
     buttonState = input("Do you pick up the item?")
-    buttonQuery(buttonState, items, "stick", "powerfull")
+    pickUpOption(buttonState, items, "stick", "powerfull")
     print(items)
     print("a LARGE \n ENEMY \n SPIDER...\n appears")
 
     userDesicion = input("what will you do??")
     
-    
-    
-
