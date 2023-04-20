f5e79f4506a7ff7f0eb4ecd45e8dc23e61fb0feb
Anasaii <heroescoulddie@gmail.com>
2023-04-20 14:06:48 -0400

    remote commit

---
+++ Combat demo.py
@@ -0,0 +1,34 @@
+#Attack, Defend, Item, Escape
+
+health = 100
+items = {}
+weapons = {}
+
+def buttonQuery(buttonState, list, listname, listdesc):
+    if buttonState == "yes":
+        list[listname] = listdesc
+        buttonState = None
+    else:
+        buttonState = None
+        pass
+
+def spider.self
+class Game:
+    print("You find a weapon!")
+
+    buttonState = input("Do you pick up the weapon?")
+    buttonQuery(buttonState, weapons, "flimsyWeapon", 50)
+
+    print(weapons)
+    print("magically you also find stick")
+
+    buttonState = input("Do you pick up the item?")
+    buttonQuery(buttonState, items, "stick", "powerfull")
+    print(items)
+    print("a LARGE \n ENEMY \n SPIDER...\n appears")
+
+    userDesicion = input("what will you do??")
+    
+    
+    
+
