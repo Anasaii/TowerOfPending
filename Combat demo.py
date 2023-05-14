31180e90b6b5945a9abb4f7df6ef85eed0c96e81
Anasaii <heroescoulddie@gmail.com>
2023-05-14 15:15:10 -0400

    deathcheck, avp, pickup, enemy addition

--- Combat demo.py
+++ Combat demo.py
@@ -1,10 +1,24 @@
 #Attack, Defend, Item, Escape
 
-health = 100
+pHealth = 100
+remainingHealth = None
 items = {}
 weapons = {}
 
-def buttonQuery(buttonState, list, listname, listdesc):
+def deathCheck(pHealth):
+    if pHealth < 0: 
+        print("Death TRUE") 
+    else:
+        print("Death FALSE")
+
+def avp(damageDone, health):
+    global pHealth
+    remainingHealth = health - damageDone
+    print("You have " + str(remainingHealth) + " health remaining...")
+    pHealth = remainingHealth
+    remainingHealth = None
+
+def pickUpOption(buttonState, list, listname, listdesc):
     if buttonState == "yes":
         list[listname] = listdesc
         buttonState = None
@@ -12,24 +26,42 @@ def buttonQuery(buttonState, list, listname, listdesc):
         buttonState = None
         pass
 
-def spider.self()
+
+class enemy():
+    def __init__(self, health, damage):
+        self.health = health
+        self.damage = damage
+
+    def attack(abc):
+        print("Hello i attack you for " + str(abc.damage))
+        avp(abc.damage, pHealth)
+        deathCheck(pHealth)
+
+
+spider = enemy(5,19)    
 
 class Game:
-    print("You find a weapon!")
+    gamestate = True
+    if gamestate == True:
+        print("You find a weapon!")
+        
+        buttonState = input("Do you pick up the weapon?")
+        pickUpOption(buttonState, weapons, "flimsyWeapon", 50)
 
-    buttonState = input("Do you pick up the weapon?")
-    buttonQuery(buttonState, weapons, "flimsyWeapon", 50)
+        print(weapons)
+        print("magically you also find stick")
 
-    print(weapons)
-    print("magically you also find stick")
+        buttonState = input("Do you pick up the item?")
+        pickUpOption(buttonState, items, "stick", "powerfull")
+        print(items)
+        print("a LARGE \n ENEMY \n SPIDER...\n appears")
 
-    buttonState = input("Do you pick up the item?")
-    buttonQuery(buttonState, items, "stick", "powerfull")
-    print(items)
-    print("a LARGE \n ENEMY \n SPIDER...\n appears")
+        userDesicion = input("what will you do??")
+        print("ah you took too long to think, spider attacks!")
+        spider.attack()
+        spider.attack()
+        spider.attack()
+        spider.attack()
 
-    userDesicion = input("what will you do??")
-    
-    
-    
 
+          
