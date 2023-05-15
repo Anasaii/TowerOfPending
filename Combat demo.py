59166312f57298aad1801d937acd93b04be94d5f
Anasaii <heroescoulddie@gmail.com>
2023-05-15 15:24:06 -0400

    broken but heres my progress

--- Combat demo.py
+++ Combat demo.py
@@ -1,67 +1,149 @@
 #Attack, Defend, Item, Escape
+import random
 
 pHealth = 100
-remainingHealth = None
 items = {}
-weapons = {}
+weapons = {} 
+equipedWeapon = weapons
+defenseFlag = False
+turn = 0
 
-def deathCheck(pHealth):
-    if pHealth < 0: 
+
+#check death status for player
+def deathCheck(health):
+    if health <= 0: 
         print("Death TRUE") 
     else:
         print("Death FALSE")
 
-def avp(damageDone, health):
+#takes health, checks if defended, damages player, sets health
+def attack_and_calculate_remaining_health_player(damage, health):
     global pHealth
-    remainingHealth = health - damageDone
-    print("You have " + str(remainingHealth) + " health remaining...")
-    pHealth = remainingHealth
-    remainingHealth = None
-
-def pickUpOption(buttonState, list, listname, listdesc):
-    if buttonState == "yes":
-        list[listname] = listdesc
-        buttonState = None
+    if defenseFlag == False :
+        remainingHealth = health - damage
     else:
-        buttonState = None
-        pass
+        remainingHealth = health - damage / defenseMod
+    print("You have " + str(remainingHealth) + " health remaining... \n")
+    pHealth = remainingHealth
+    
+#if prompted yes, the user will pick up the item
+def pickUpOption(buttonState, list, name, description):
+    if buttonState == "yes":
+        list[name] = description
 
 
-class enemy():
-    def __init__(self, health, damage):
+#enemy class given name, health and its damage
+class Enemy:
+    def __init__(self,entity, health, damage):
+        self.entity = entity
         self.health = health
         self.damage = damage
+#states name, states damage cost and remaining health, if death is active
+    def attack_to_player(self):
+        print(self.entity + " attacks you for " + str(self.damage))
+        attack_and_calculate_remaining_health_player(self.damage, pHealth)
+        #deathCheck(pHealth)
 
-    def attack(abc):
-        print("Hello i attack you for " + str(abc.damage))
-        avp(abc.damage, pHealth)
-        deathCheck(pHealth)
+#
+class Menu:
+    def __init__(self, equipedWeapon, items, health, enemyHealth=5):
+        self.equippedWeapon = equipedWeapon
+        self.items = items
+        self.health = health
+        self.enemyHealth = enemyHealth
+    #checks if weapon is set, then inflicts damage based on weapon, if not skips.
+    def attack_to_enemy(self):
+
+        if self.equippedWeapon:
+            attackDamage = int(list(equipedWeapon.values())[0])
+            inflictedDamage = self.enemyHealth - attackDamage
+            print(inflictedDamage)
+        else:
+            print("You have no weapons!")
+            
+    #raises the global defense flag for next attack against player
+    def defend(self):
+        global defenseFlag, defenseMod
+        print("You brace for the next attack.")
+        defenseFlag = True
+        defenseMod = 2
+
+    #T O D O
+    def item(self):
+        print(items)
+
+    #50/50 chance to escape, guaranteed escape on turn 3
+    def escape(self):
+        escapeOption = False
+        if Game.round <= 3:
+            escapeOption = True
+        else:
+            escapeGacha = random.choice[0,1]
+            if escapeGacha == 0:
+                escapeOption = True
+
+        print(escapeOption)
+
+class Player:
+    def __init__(self, name) -> None:
+        self.name = name
 
 
-spider = enemy(5,19)    
+    def take_turn(self):
+        Menu1 = Menu(equipedWeapon,items,pHealth)
+        selection = input("1.Attack\n2.Defend\n3.Item\n4.Escape")
+        match selection:
+            case "Attack":
+                Menu1.attack_to_enemy()
+                print(equipedWeapon)
+            case "Defend":
+                Menu1.defend()
+            case "Item":
+                Menu1.item()
+            case "Escape":
+                Menu1.escape()
+            case _:
+                pass
+
+
+
+spider = Enemy("Spider", 5, 19)
+smallSnake = Enemy("Small Snake", 3,6)
+
+
+def prematch():
+    print("You find a weapon!")
+        
+    buttonState = input("Do you pick up the weapon?")
+    pickUpOption(buttonState, weapons, "flimsyWeapon", 50)
+
+    print(weapons)
+    print("Magically, you also find a stick.")
+
+    buttonState = input("Do you pick up the item?")
+    pickUpOption(buttonState, items, "stick", "powerful")
+    print(items)
+    print("A LARGE\nENEMY\nSPIDER...\nappears")
+
+    print("Ah, you took too long to think. The spider attacks!\n")
+    spider.attack_to_player()
 
 class Game:
-    gamestate = True
-    if gamestate == True:
-        print("You find a weapon!")
+    game_over = False
+    enemyParty = {spider, smallSnake}
+    
+    def __init__(self):
+        self.game_over = False
+        self.round = 0
+
+    def new_round(self):
+        self.round += 1
+        print(f"\n***   Round: {self.round}   ***\n")
+
+    prematch()
+    while game_over == False:
+        jess = Player("Jess")
+        jess.take_turn()
         
-        buttonState = input("Do you pick up the weapon?")
-        pickUpOption(buttonState, weapons, "flimsyWeapon", 50)
-
-        print(weapons)
-        print("magically you also find stick")
-
-        buttonState = input("Do you pick up the item?")
-        pickUpOption(buttonState, items, "stick", "powerfull")
-        print(items)
-        print("a LARGE \n ENEMY \n SPIDER...\n appears")
-
-        userDesicion = input("what will you do??")
-        print("ah you took too long to think, spider attacks!")
-        spider.attack()
-        spider.attack()
-        spider.attack()
-        spider.attack()
 
 
-          
